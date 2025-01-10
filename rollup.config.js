831afa07d32ed9e45c32caccf8fda467c1a284a5
Adam Zerner <adamzerner@protonmail.com>
2025-01-09 16:59:17 -0800

    Continue working on setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import typescript from "@rollup/plugin-typescript";
-import packageJson from "./package.json";
+import packageJson from "./package.json" with { type: "json" };
 
 export default {
   input: "src/index.tsx",
