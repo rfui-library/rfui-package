86d2ae2265db0ac032ec5458bbc5b353a4dc92f7
Adam Zerner <adamzerner@protonmail.com>
2025-01-13 20:29:08 -0800

    Tweak

--- src/form/input.tsx
+++ src/form/input.tsx
@@ -24,35 +24,31 @@ export const Input = ({
   let className =
     "w-full border border-neutral-500 px-2 py-1 focus:border-neutral-900 focus:shadow-sm focus:outline-none";
 
-  className +=
-    " " +
-    (() => {
-      switch (size) {
-        case "sm":
-          return "px-2 text-sm";
-        case "md":
-          return "px-3 py-2";
-        case "lg":
-          return "px-4 py-3 text-lg";
-      }
-    })();
+  className += (() => {
+    switch (size) {
+      case "sm":
+        return " px-2 text-sm";
+      case "md":
+        return " px-3 py-2";
+      case "lg":
+        return " px-4 py-3 text-lg";
+    }
+  })();
 
-  className +=
-    " " +
-    (() => {
-      switch (rounded) {
-        case "square":
-          return "rounded-none";
-        case "sm":
-          return "rounded";
-        case "lg":
-          return "rounded-lg";
-        case "full":
-          return "rounded-full px-3";
-        default:
-          return "rfui-rounded-default";
-      }
-    })();
+  className += (() => {
+    switch (rounded) {
+      case "square":
+        return " rounded-none";
+      case "sm":
+        return " rounded";
+      case "lg":
+        return " rounded-lg";
+      case "full":
+        return " rounded-full px-3";
+      default:
+        return " rfui-rounded-default";
+    }
+  })();
 
   if (rest.disabled) {
     className += " cursor-not-allowed bg-neutral-50";
