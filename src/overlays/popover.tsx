ad4f6b381a5955a75a696139c8282e395552581d
Adam Zerner <adamzerner@protonmail.com>
2025-01-24 18:18:45 -0800

    Do initial work on `Popover` component

--- src/overlays/popover.tsx
+++ src/overlays/popover.tsx
@@ -1,33 +1,28 @@
-// import {
-//   Popover as HeadlessUIPopover,
-//   PopoverButton,
-//   PopoverPanel,
-// } from "@headlessui/react";
-// import type { ReactNode, ComponentProps } from "react";
+import {
+  Popover as HeadlessUIPopover,
+  PopoverButton,
+  PopoverPanel,
+} from "@headlessui/react";
+import type { ReactNode } from "react";
 
-// export type PopoverType = {
-//   content: string;
-//   children: ReactNode;
-// } & ComponentProps<"span">;
+export type PopoverType = {
+  content: string;
+  children: ReactNode;
+};
 
-// /** *
-//  * @function Popover
-//  *
-//  * @see {@link https://rfui-docs.onrender.com/components/overlays/popover}
-//  *
-//  * @example
-//  * <Popover content="Example content">Click me</Popover>
-//  */
-// export const Popover = ({ content, children, ...rest }: PopoverType) => {
-//   return (
-//     <HeadlessUIPopover className="relative">
-//       <PopoverButton>Solutions</PopoverButton>
-//       <PopoverPanel anchor="bottom" className="flex flex-col">
-//         <a href="/analytics">Analytics</a>
-//         <a href="/engagement">Engagement</a>
-//         <a href="/security">Security</a>
-//         <a href="/integrations">Integrations</a>
-//       </PopoverPanel>
-//     </HeadlessUIPopover>
-//   );
-// };
+/** *
+ * @function Popover
+ *
+ * @see {@link https://rfui-docs.onrender.com/components/overlays/popover}
+ *
+ * @example
+ * <Popover content="Example content">Click me</Popover>
+ */
+export const Popover = ({ content, children }: PopoverType) => {
+  return (
+    <HeadlessUIPopover className="relative">
+      <PopoverButton>{children}</PopoverButton>
+      <PopoverPanel anchor="bottom">{content}</PopoverPanel>
+    </HeadlessUIPopover>
+  );
+};
