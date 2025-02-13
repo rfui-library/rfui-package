2bbd1a0a472409a4b797f41c06f1b0102e309461
Adam Zerner <adamzerner@protonmail.com>
2025-02-13 01:06:51 -0800

    Make `FormField` a wrapper

--- src/form/form-field.tsx
+++ src/form/form-field.tsx
@@ -1,68 +1,17 @@
-import { XCircleIcon } from "@heroicons/react/24/outline";
-import type { ComponentProps } from "react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import type { ComponentProps, ReactNode } from "react";
 import { useId } from "react";
-import { Checkbox } from "../form/checkbox";
-import { Input } from "../form/input";
-import { PasswordInput } from "../form/password-input";
-import type { RadioButtonGroupType } from "../form/radio-button-group";
-import {
-  RadioButtonGroup,
-  RadioButtonGroupItem,
-} from "../form/radio-button-group";
-import { Select, type SelectType } from "../form/select";
-import { Switch } from "../form/switch";
-import type { TextareaType } from "../form/textarea";
-import { Textarea } from "../form/textarea";
 import { Flex } from "../layout/flex";
-
-type ExcludedInputProps =
-  | "name"
-  | "value"
-  | "type"
-  | "required"
-  | "size"
-  | "rounded"
-  | "invalid"
-  | "onChange"
-  | "onInput";
 
 export type FormFieldType = {
   label: string;
-  name?: ComponentProps<"input">["name"];
-  value?: ComponentProps<"input">["value"];
-  defaultValue?: ComponentProps<"input">["defaultValue"];
-  selectDefaultValue?: SelectType["defaultValue"];
-  checked?: boolean;
-  defaultChecked?: boolean;
-  type?:
-    | ComponentProps<"input">["type"]
-    | "switch"
-    | "rfui-password-input"
-    | "textarea"
-    | "radio-button-group"
-    | "select";
   required?: boolean;
   requiredIndicator?: "text" | "asterisk" | "none";
   optionalIndicator?: "text" | "asterisk" | "none";
   helperText?: string;
   size?: "sm" | "md" | "lg";
-  rounded?: "square" | "sm" | "lg" | "full";
-  invalid?: boolean;
   errorText?: string;
-  radioButtonGroupOptions?: {
-    value: string;
-    display: string;
-  }[];
-  onChange?: (e: any) => void;
-  onInput?: (e: any) => void;
-  inputRest?: Omit<ComponentProps<"input">, ExcludedInputProps>;
-  textareaRest?: Omit<TextareaType, ExcludedInputProps>;
-  selectOptions?: SelectType["options"];
-  radioButtonGroupRest?: Omit<
-    RadioButtonGroupType,
-    ExcludedInputProps | "children"
-  >;
-  selectRest?: SelectType;
+  children: ReactNode;
 } & Omit<ComponentProps<"div">, "size">;
 
 /** *
@@ -73,33 +22,19 @@ export type FormFieldType = {
  * @param requiredIndicator See https://ux.stackexchange.com/q/840/39046 for a discussion.
  *
  * @example
- * <FormField label="Name" />
+ * <FormField label="Name">
+ *   <Input name="name" />
+ * </FormField>
  */
 export const FormField = ({
   label,
-  name,
-  value,
-  defaultValue,
-  selectDefaultValue,
-  checked,
-  defaultChecked,
-  type,
   required = false,
   requiredIndicator = "none",
   optionalIndicator = "none",
   helperText,
   size = "md",
-  rounded,
-  invalid = false,
   errorText,
-  radioButtonGroupOptions,
-  onChange,
-  onInput,
-  inputRest,
-  textareaRest,
-  radioButtonGroupRest,
-  selectOptions,
-  selectRest,
+  children,
   ...rest
 }: FormFieldType) => {
   const id = useId();
@@ -113,14 +48,6 @@ export const FormField = ({
         return ["text-base", "text-lg"];
     }
   })();
-  const { className: inputRestClassName, ...inputRestWithoutClassName } =
-    inputRest || {};
-  const { className: textareaRestClassName, ...textareaRestWithoutClassName } =
-    textareaRest || {};
-  const {
-    className: radioButtonGroupRestClassName,
-    ...radioButtonGroupRestWithoutClassName
-  } = radioButtonGroupRest || {};
 
   return (
     <div {...rest}>
@@ -142,140 +69,17 @@ export const FormField = ({
       <div className={`${smallFontClass} text-neutral-700 mb-1`}>
         {helperText}
       </div>
-      {invalid && errorText && (
+      {errorText && (
         <Flex
           className={`${smallFontClass} text-supporting-red-700 mb-1 gap-1 items-center`}
         >
-          <XCircleIcon
-            className={size === "sm" ? "w-4 h-4 inline" : "w-5 h-5 inline"}
+          <ExclamationTriangleIcon
+            className={size === "sm" ? "w-3 h-3 inline" : "w-4 h-4 inline"}
           />{" "}
           {errorText}
         </Flex>
       )}
-      {type === "checkbox" ? (
-        <Checkbox
-          id={id}
-          name={name}
-          value={value}
-          checked={checked}
-          defaultChecked={defaultChecked}
-          required={required}
-          size={size}
-          invalid={invalid}
-          className={inputRestClassName ? `mt-1 ${inputRestClassName}` : "mt-1"}
-          onChange={onChange}
-          onInput={onInput}
-          {...inputRestWithoutClassName}
-        />
-      ) : type === "switch" ? (
-        <Switch
-          id={id}
-          name={name}
-          value={value}
-          checked={checked}
-          defaultChecked={defaultChecked}
-          required={required}
-          className={inputRestClassName ? `mt-1 ${inputRestClassName}` : "mt-1"}
-          onChange={onChange}
-          onInput={onInput}
-          {...inputRestWithoutClassName}
-        />
-      ) : type === "rfui-password-input" ? (
-        <PasswordInput
-          id={id}
-          name={name}
-          value={value}
-          defaultValue={defaultValue}
-          required={required}
-          size={size}
-          rounded={rounded}
-          invalid={invalid}
-          className={
-            inputRestClassName
-              ? `block w-full ${inputRestClassName}`
-              : "block w-full"
-          }
-          onChange={onChange}
-          onInput={onInput}
-          {...inputRestWithoutClassName}
-        />
-      ) : type === "textarea" ? (
-        <Textarea
-          id={id}
-          name={name}
-          value={value}
-          defaultValue={defaultValue}
-          required={required}
-          invalid={invalid}
-          className={
-            textareaRestClassName
-              ? `block w-full ${textareaRestClassName}`
-              : "block w-full"
-          }
-          onChange={onChange}
-          onInput={onInput}
-          {...textareaRestWithoutClassName}
-        />
-      ) : type === "radio-button-group" && radioButtonGroupOptions ? (
-        <RadioButtonGroup
-          id={id}
-          name={name as string}
-          className={
-            radioButtonGroupRestClassName
-              ? `block w-full mt-3 ${radioButtonGroupRestClassName}`
-              : "block w-full mt-3"
-          }
-          onChange={
-            onChange
-              ? (newVal) => {
-                  onChange({
-                    target: {
-                      value: newVal,
-                    },
-                  });
-                }
-              : undefined
-          }
-          {...radioButtonGroupRestWithoutClassName}
-        >
-          {radioButtonGroupOptions.map(({ value, display }) => (
-            <RadioButtonGroupItem value={value} key={value}>
-              {display}
-            </RadioButtonGroupItem>
-          ))}
-        </RadioButtonGroup>
-      ) : type === "select" ? (
-        <Select
-          options={selectOptions ?? []}
-          name={name}
-          defaultValue={selectDefaultValue}
-          size={size}
-          rounded={rounded}
-          invalid={invalid}
-          onChange={onChange}
-          {...selectRest}
-        />
-      ) : (
-        <Input
-          id={id}
-          name={name}
-          value={value}
-          defaultValue={defaultValue}
-          type={type}
-          required={required}
-          size={size}
-          rounded={rounded}
-          invalid={invalid}
-          className={
-            inputRestClassName
-              ? `block w-full ${inputRestClassName}`
-              : "block w-full"
-          }
-          onChange={onChange}
-          onInput={onInput}
-          {...inputRestWithoutClassName}
-        />
-      )}
+      {children}
     </div>
   );
 };
