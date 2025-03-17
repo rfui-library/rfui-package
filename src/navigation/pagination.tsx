c10ecf24f3f1274aefe4511f703216f3edc49017
Adam Zerner <adamzerner@protonmail.com>
2025-03-16 18:31:01 -0700

    Handle controlled and uncontrolled approaches

--- src/navigation/pagination.tsx
+++ src/navigation/pagination.tsx
@@ -5,6 +5,8 @@ export type PaginationType = {
   currPage: number;
   itemsPerPage: number;
   totalItems: number;
+  buildHref?: (page: number) => string;
+  onChange?: (newPage: number) => void;
 } & Omit<ComponentProps<"nav">, "size">;
 
 /** *
@@ -19,6 +21,8 @@ export const Pagination = ({
   currPage,
   itemsPerPage,
   totalItems,
+  buildHref,
+  onChange,
   ...rest
 }: PaginationType) => {
   const { className: restClass, ...restWithoutClass } = rest;
@@ -37,25 +41,73 @@ export const Pagination = ({
   return (
     <nav className={className} {...restWithoutClass}>
       {currPage > 1 && (
-        <PaginationItem>
+        <PaginationItem
+          page={currPage - 1}
+          buildHref={buildHref}
+          onChange={onChange}
+        >
           <ChevronLeftIcon className="size-4" />
         </PaginationItem>
       )}
-      {currPage !== 1 && <PaginationItem>1</PaginationItem>}
+      {currPage !== 1 && (
+        <PaginationItem page={1} buildHref={buildHref} onChange={onChange}>
+          1
+        </PaginationItem>
+      )}
       {lastPage > 7 && currPage > 4 && <Elipsis />}
-      {currPage - 2 > 1 && <PaginationItem>{currPage - 2}</PaginationItem>}
-      {currPage - 1 > 1 && <PaginationItem>{currPage - 1}</PaginationItem>}
+      {currPage - 2 > 1 && (
+        <PaginationItem
+          page={currPage - 2}
+          buildHref={buildHref}
+          onChange={onChange}
+        >
+          {currPage - 2}
+        </PaginationItem>
+      )}
+      {currPage - 1 > 1 && (
+        <PaginationItem
+          page={currPage - 1}
+          buildHref={buildHref}
+          onChange={onChange}
+        >
+          {currPage - 1}
+        </PaginationItem>
+      )}
       {<ActivePaginationItem>{currPage}</ActivePaginationItem>}
       {currPage + 1 < lastPage && (
-        <PaginationItem>{currPage + 1}</PaginationItem>
+        <PaginationItem
+          page={currPage + 1}
+          buildHref={buildHref}
+          onChange={onChange}
+        >
+          {currPage + 1}
+        </PaginationItem>
       )}
       {currPage + 2 < lastPage && (
-        <PaginationItem>{currPage + 2}</PaginationItem>
+        <PaginationItem
+          page={currPage + 2}
+          buildHref={buildHref}
+          onChange={onChange}
+        >
+          {currPage + 2}
+        </PaginationItem>
       )}
       {lastPage > 7 && currPage < lastPage - 3 && <Elipsis />}
-      {currPage !== lastPage && <PaginationItem>{lastPage}</PaginationItem>}
+      {currPage !== lastPage && (
+        <PaginationItem
+          page={lastPage}
+          buildHref={buildHref}
+          onChange={onChange}
+        >
+          {lastPage}
+        </PaginationItem>
+      )}
       {currPage < lastPage && (
-        <PaginationItem>
+        <PaginationItem
+          page={currPage + 1}
+          buildHref={buildHref}
+          onChange={onChange}
+        >
           <ChevronRightIcon className="size-4" />
         </PaginationItem>
       )}
@@ -63,11 +115,37 @@ export const Pagination = ({
   );
 };
 
-const PaginationItem = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="rfui-rounded-default flex h-[2.5rem] cursor-pointer items-center px-3 py-2 hover:bg-neutral-50">
+const PaginationItem = ({
+  page,
+  buildHref,
+  onChange,
+  children,
+}: {
+  page: number;
+  buildHref?: PaginationType["buildHref"];
+  onChange?: PaginationType["onChange"];
+  children: ReactNode;
+}) => {
+  const sharedClassName =
+    "rfui-rounded-default flex h-[2.5rem] items-center px-3 py-2 hover:bg-neutral-50";
+  const linkClassName = `${sharedClassName}`;
+  const buttonClassName = `${sharedClassName} cursor-pointer`;
+
+  return buildHref ? (
+    <a href={buildHref(page)} className={linkClassName}>
       {children}
-    </div>
+    </a>
+  ) : (
+    <button
+      onClick={() => {
+        if (onChange) {
+          onChange(page);
+        }
+      }}
+      className={buttonClassName}
+    >
+      {children}
+    </button>
   );
 };
 
