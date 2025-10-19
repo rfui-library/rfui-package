e8885de0a881037768ffeb8072ee3d9630d5c249
Adam Zerner <adamzerner@protonmail.com>
2025-10-18 17:49:47 -0700

    WIP

--- src/form/input.tsx
+++ src/form/input.tsx
@@ -1,9 +1,12 @@
-import type { ComponentProps } from "react";
+import type { ComponentProps, ReactNode } from "react";
+import { Flex } from "../layout/flex";
 
 export type InputType = {
   size?: "sm" | "md" | "lg";
   rounded?: "square" | "sm" | "lg" | "full";
   invalid?: boolean;
+  contentBefore?: ReactNode;
+  contentAfter?: ReactNode;
 } & Omit<ComponentProps<"input">, "size">;
 
 /** *
@@ -18,6 +21,8 @@ export const Input = ({
   size = "md",
   rounded,
   invalid = false,
+  contentBefore,
+  contentAfter,
   ...rest
 }: InputType) => {
   const { className: restClass, ...restWithoutClass } = rest;
@@ -64,8 +69,34 @@ export const Input = ({
       " border-neutral-500 hover:border-neutral-900 focus:border-primary-900";
   }
 
+  if (contentBefore) {
+    className += " border-l-0 rounded-l-none!";
+  }
+
+  if (contentAfter) {
+    className += " border-r-0 rounded-r-none!";
+  }
+
   if (restClass) {
     className += ` ${restClass}`;
+  }
+
+  if (contentBefore || contentAfter) {
+    return (
+      <Flex className="items-stretch">
+        {contentBefore && (
+          <div className="rounded-l-sm rounded-r-none border border-r-0 border-neutral-500 bg-neutral-50 px-3 py-2">
+            {contentBefore}
+          </div>
+        )}
+        <input className={className} {...restWithoutClass} />
+        {contentAfter && (
+          <div className="rounded-l-none rounded-r-sm border border-l-0 border-neutral-500 bg-neutral-50 px-3 py-2">
+            {contentAfter}
+          </div>
+        )}
+      </Flex>
+    );
   }
 
   return <input className={className} {...restWithoutClass} />;
