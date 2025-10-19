1f711df2cd265d71e8a2715f6f3a8a93f800454c
Adam Zerner <adamzerner@protonmail.com>
2025-10-18 19:55:25 -0700

    Content before after input (#26)
    * WIP
    
    * Make more dynamic
    
    * Tweak
    
    * Work on outline

--- src/form/input.tsx
+++ src/form/input.tsx
@@ -1,9 +1,12 @@
-import type { ComponentProps } from "react";
+import type { ComponentProps, ReactNode } from "react";
+import { Flex } from "../layout/flex";
 
 export type InputType = {
   size?: "sm" | "md" | "lg";
   rounded?: "square" | "sm" | "lg" | "full";
   invalid?: boolean;
+  contentBefore?: ReactNode;
+  contentAfter?: ReactNode;
 } & Omit<ComponentProps<"input">, "size">;
 
 /** *
@@ -18,35 +21,52 @@ export const Input = ({
   size = "md",
   rounded,
   invalid = false,
+  contentBefore,
+  contentAfter,
   ...rest
 }: InputType) => {
   const { className: restClass, ...restWithoutClass } = rest;
-  let className = "w-full border px-2 py-1 hover:shadow-sm focus:shadow-md";
+  let className = "z-1 w-full border px-2 py-1 hover:shadow-sm focus:shadow-md";
+  let contentBeforeClassName =
+    "rounded-r-none border border-r-0 border-neutral-500 bg-neutral-50";
+  let contentAfterClassName =
+    "rounded-l-none border border-l-0 border-neutral-500 bg-neutral-50";
 
-  className += (() => {
-    switch (size) {
-      case "sm":
-        return " px-2 text-sm";
-      case "md":
-        return " px-3 py-2";
-      case "lg":
-        return " px-4 py-3 text-lg";
-    }
-  })();
-  className += (() => {
-    switch (rounded) {
-      case "square":
-        return " rounded-none";
-      case "sm":
-        return " rounded-sm";
-      case "lg":
-        return " rounded-lg";
-      case "full":
-        return " rounded-full px-3";
-      default:
-        return " rfui-rounded-default";
-    }
-  })();
+  if (size === "sm") {
+    className += " px-2 text-sm";
+    contentBeforeClassName += " px-2 text-sm";
+    contentAfterClassName += " px-2 text-sm";
+  } else if (size === "md") {
+    className += " px-3 py-2";
+    contentBeforeClassName += " px-3 py-2";
+    contentAfterClassName += " px-3 py-2";
+  } else if (size === "lg") {
+    className += " px-4 py-3 text-lg";
+    contentBeforeClassName += " px-4 py-3 text-lg";
+    contentAfterClassName += " px-4 py-3 text-lg";
+  }
+
+  if (rounded === "square") {
+    className += " rounded-none";
+    contentBeforeClassName += " rounded-none";
+    contentAfterClassName += " rounded-none";
+  } else if (rounded === "sm") {
+    className += " rounded-sm";
+    contentBeforeClassName += " rounded-sm";
+    contentAfterClassName += " rounded-sm";
+  } else if (rounded === "lg") {
+    className += " rounded-lg";
+    contentBeforeClassName += " rounded-lg";
+    contentAfterClassName += " rounded-lg";
+  } else if (rounded === "full") {
+    className += " rounded-full";
+    contentBeforeClassName += " rounded-full";
+    contentAfterClassName += " rounded-full";
+  } else {
+    className += " rfui-rounded-default";
+    contentBeforeClassName += " rfui-rounded-default";
+    contentAfterClassName += " rfui-rounded-default";
+  }
 
   if (rest.disabled) {
     className += " cursor-not-allowed bg-neutral-50";
@@ -58,14 +78,36 @@ export const Input = ({
 
   if (invalid) {
     className +=
-      " border-supporting-red-300 bg-supporting-red-50 text-supporting-red-900 focus:border-supporting-red-700";
+      " border-supporting-red-300 bg-supporting-red-50 text-supporting-red-900 focus:border-supporting-red-700 focus:outline-1 focus-visible:outline-supporting-red-700";
   } else {
     className +=
       " border-neutral-500 hover:border-neutral-900 focus:border-primary-900";
   }
 
+  if (contentBefore) {
+    className += " border-l-0 rounded-l-none!";
+  }
+
+  if (contentAfter) {
+    className += " border-r-0 rounded-r-none!";
+  }
+
   if (restClass) {
     className += ` ${restClass}`;
+  }
+
+  if (contentBefore || contentAfter) {
+    return (
+      <Flex className="items-stretch">
+        {contentBefore && (
+          <div className={contentBeforeClassName}>{contentBefore}</div>
+        )}
+        <input className={className} {...restWithoutClass} />
+        {contentAfter && (
+          <div className={contentAfterClassName}>{contentAfter}</div>
+        )}
+      </Flex>
+    );
   }
 
   return <input className={className} {...restWithoutClass} />;
