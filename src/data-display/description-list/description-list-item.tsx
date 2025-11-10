7b58c1a3a0c75d06f6dfefea2805350fac36615a
Adam Zerner <adamzerner@protonmail.com>
2025-11-10 10:36:40 -0800

    Add rest to `DescriptionListItem`

--- src/data-display/description-list/description-list-item.tsx
+++ src/data-display/description-list/description-list-item.tsx
@@ -1,16 +1,17 @@
-import { ReactNode } from "react";
+import { ComponentProps, ReactNode } from "react";
 
 export type DescriptionListItemType = {
   label: string;
   children: ReactNode;
-};
+} & ComponentProps<"div">;
 
 export const DescriptionListItem = ({
   label,
   children,
+  ...rest
 }: DescriptionListItemType) => {
   return (
-    <div>
+    <div {...rest}>
       <dt className="mb-1 text-sm text-neutral-700">{label}</dt>
       <dd>{children}</dd>
     </div>
