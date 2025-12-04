687457187a5026ab065cc7d5db67a2b3e95c4324
Adam Zerner <adamzerner@protonmail.com>
2025-12-03 16:11:47 -0800

    Add `dtRest` and `ddRest`

--- src/data-display/description-list/description-list-item.tsx
+++ src/data-display/description-list/description-list-item.tsx
@@ -3,17 +3,23 @@ import { ComponentProps, ReactNode } from "react";
 export type DescriptionListItemType = {
   label: string;
   children: ReactNode;
+  dtRest?: ComponentProps<"dt">;
+  ddRest?: ComponentProps<"dd">;
 } & ComponentProps<"div">;
 
 export const DescriptionListItem = ({
   label,
   children,
+  dtRest,
+  ddRest,
   ...rest
 }: DescriptionListItemType) => {
   return (
     <div {...rest}>
-      <dt className="mb-1 text-sm text-neutral-700">{label}</dt>
-      <dd>{children}</dd>
+      <dt className="mb-1 text-sm text-neutral-700" {...dtRest}>
+        {label}
+      </dt>
+      <dd {...ddRest}>{children}</dd>
     </div>
   );
 };
